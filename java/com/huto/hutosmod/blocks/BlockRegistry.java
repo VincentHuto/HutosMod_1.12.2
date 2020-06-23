7f9daae168ee36c410fb2a688bffe0bd0dd336a3
Vincent Comaroto <amae804@gmail.com>
2020-06-23 14:00:01 -0500

    moved vanilla textures, added morel blocks

--- java/com/huto/hutosmod/blocks/BlockRegistry.java
+++ java/com/huto/hutosmod/blocks/BlockRegistry.java
@@ -32,6 +32,9 @@ public class BlockRegistry {
 	public static final Block activated_obsidian = new BlockActivatedObsidian("activated_obsidian", Material.IRON);
 	public static final Block nether_block = new BlockNetherBlock("nether_block", Material.ROCK).setHardness(5F)
 			.setResistance(15F);
+	public static final Block morel_cap = new BlockBase("morel_cap", Material.CACTUS);
+	public static final Block morel_stem = new BlockBase("morel_stem", Material.CACTUS);
+
 	public static final Block display_pedestal = new BlockDisplayPedestal("display_pedestal", Material.ROCK);
 	public static final Block wand_maker = new BlockWandMaker("wand_maker", Material.ROCK);
 	public static final Block mana_belljar = new BlockManaBelljar("mana_belljar", Material.GLASS);
